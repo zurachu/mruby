fdb7fd5e00acd073e3233f4004708be763e40c72
zurachu <zurachu@gmail.com>
2018-02-18 21:46:04 +0900

    起動時にmrb_open(), 終了時にmrb_close()するだけのサンプルプログラム
    errno, exit(), _exit() が Unresolved external symbol となるため実装している
    が、exitはP/ECEの仕様上ダメなはずなので、とりあえず無限ループさせている

---
+++ examples/targets/PIECE/hello.c
@@ -0,0 +1,50 @@
+#define _WCHAR_T
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include <piece.h>
+
+#include "mruby.h"
+
+unsigned char vbuff[128*88];
+static unsigned char draw;
+
+int errno;
+void _exit( int status ) { while(1); }
+void exit( int status ) { _exit(status); }
+
+mrb_state* mrb;
+
+void pceAppInit( void )
+{
+	pceLCDDispStop();
+	pceLCDSetBuffer( vbuff );
+	pceAppSetProcPeriod( 80 );
+	memset( vbuff, 0, 128*88 );
+
+	mrb = mrb_open();
+
+	pceFontSetPos( 0, 0);
+	pceFontPrintf("Hello, World");
+
+	draw = 1;
+
+	pceLCDDispStart();
+}
+
+
+void pceAppProc( int cnt )
+{
+	if ( draw ) {
+		pceLCDTrans();
+		draw = 0;
+	}
+}
+
+
+void pceAppExit( void )
+{
+	mrb_close(mrb);
+}
+
