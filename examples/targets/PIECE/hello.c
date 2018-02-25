e72b46fba7da04d2c79222ff16c243b943087d85
zurachu <zurachu@gmail.com>
2018-02-25 16:55:56 +0900

    mrb_open() 失敗していたので、 pceHeap～()での独自アロケータを用意

--- examples/targets/PIECE/hello.c
+++ examples/targets/PIECE/hello.c
@@ -15,6 +15,22 @@ void exit( int status ) { while(1); }
 
 mrb_state* mrb;
 
+static void* allocf(mrb_state* mrb, void* p, size_t size, void* ud)
+{
+	void* ret;
+	if(size == 0)
+	{
+		pceHeapFree(p);
+		return NULL;
+	}
+	ret = pceHeapAlloc(size);
+	if(p)
+	{
+		memcpy(ret, p, size);
+	}
+	return ret;
+}
+
 void pceAppInit( void )
 {
 	pceLCDDispStop();
@@ -22,7 +38,7 @@ void pceAppInit( void )
 	pceAppSetProcPeriod( 80 );
 	memset( vbuff, 0, 128*88 );
 
-	mrb = mrb_open();
+	mrb = mrb_open_allocf(allocf, NULL);
 
 	pceFontSetPos( 0, 0);
 	pceFontPrintf("Hello, World");
